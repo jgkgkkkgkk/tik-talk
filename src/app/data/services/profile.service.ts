64e5543bc70e6031809c018b0afd28b7a5c15440
Пашка Юрьевич <pascha1998ga@mail.ru>
2024-10-31 17:41:20 +0200

    adid serves

--- src/app/data/services/profile.service.ts
+++ src/app/data/services/profile.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
-
+http=inject(HttpClient);
   constructor() { }
+baseApiUrl='https://icherniakov.ru/yt-course/'
+  getTestAccounts(){
+
+ return this.http.get(`${this.baseApiUrl}account/test_accounts`)
+  }
 }
